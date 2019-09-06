ff998ba5cde7a78ec23bf70db69f027a8827ae00
John O Sullivan <7268419+Pattern-Projects@users.noreply.github.com>
2019-09-06 13:15:57 +0100

    Flask app routing

--- app.py
+++ app.py
@@ -6,3 +6,8 @@ app = Flask(__name__)
 @app.route('/')
 def test():
     return 'Ello world'
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT', 8000)),
+            debug=True)
