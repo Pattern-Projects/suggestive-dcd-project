f8555858e4a4d23ae79e6288bf5b91f7a206f5a3
John O Sullivan <7268419+Pattern-Projects@users.noreply.github.com>
2019-09-06 13:41:19 +0100

    Add page routes

--- app.py
+++ app.py
@@ -4,12 +4,30 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return 'Home'
+@app.route('/books')
+def books():
+    return 'Books List Page'
 
-@app.route('/second')
-def second():
-    return 'Second Page'
+@app.route('/suggest')
+def suggest():
+    return 'Suggest A Book Page'
+
+@app.route('/reading')
+def reading():
+    return 'Reading List Page'
+
+@app.route('/complete')
+def complete():
+    return 'Complete A Book Page'
+
+@app.route('/reviews')
+def reviews():
+    return 'Reviewed List Page'
+
+
+
+
+
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
